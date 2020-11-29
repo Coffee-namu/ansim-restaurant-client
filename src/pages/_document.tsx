8757188f73e32e715e6c53da3bb314af05361d31
jhaemin <io@jhaemin.com>
2020-11-29 17:13:39 +0900

    fix: resolve styled-components ssr issue

---
+++ src/pages/_document.tsx
@@ -0,0 +1,32 @@
+import Document, { DocumentContext } from 'next/document'
+import { ServerStyleSheet } from 'styled-components'
+import flush from 'styled-jsx/server'
+
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const sheet = new ServerStyleSheet()
+    const originalRenderPage = ctx.renderPage
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        })
+
+      const initialProps = await Document.getInitialProps(ctx)
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+            {flush() || null}
+          </>
+        ),
+      }
+    } finally {
+      sheet.seal()
+    }
+  }
+}
